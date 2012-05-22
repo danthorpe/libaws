b17dc599660182a0673bd7ed5500134d66e3324d
Daniel Thorpe <dan@blindingskies.com>
2012-05-22 15:24:24 +0100

    Initial Commit

---
+++ Amazon iOS SDK/Amazon_iOS_SDK.m
@@ -0,0 +1,13 @@
+//
+//  Amazon_iOS_SDK.m
+//  Amazon iOS SDK
+//
+//  Created by Daniel Thorpe on 22/05/2012.
+//  Copyright (c) 2012 Blinding Skies Limited. All rights reserved.
+//
+
+#import "Amazon_iOS_SDK.h"
+
+@implementation Amazon_iOS_SDK
+
+@end
